ac7ed1dbe1f5729f07e99697d8017f8d87d37d83
Qianxi Chen <qc815@nyu.edu>
2023-03-04 18:38:24 -0500

    finished draft for part 1 and part 2

---
+++ part2/air_ticket_reservation_inserts.sql
@@ -0,0 +1,38 @@
+DELETE FROM
+    airline;
+
+DELETE FROM
+    airline_staff;
+
+DELETE FROM
+    airline_staff_phone_number;
+
+DELETE FROM
+    airline_staff_email_address;
+
+DELETE FROM
+    airplane;
+
+DELETE FROM
+    airport;
+
+DELETE FROM
+    flight;
+
+DELETE FROM
+    customer;
+
+DELETE FROM
+    ticket;
+
+DELETE FROM
+    purchased;
+
+DELETE FROM
+    rate;
+
+--make sure the tables are clean
+INSERT INTO
+    airline
+VALUES
+    ("Jet Blue");
