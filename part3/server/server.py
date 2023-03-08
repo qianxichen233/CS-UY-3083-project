7ec0e698a7fac480434ac26ec3bab104fd4c5fa9
Xiaoyi Yan <galaxy_xy@10-18-178-227.dynapool.wireless.nyu.edu>
2023-03-08 14:18:56 -0500

    test change

--- part3/server/server.py
+++ part3/server/server.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def test():
 	return {"test": 'success'}
 
+@app.route("/test")
+def test():
+	return {"another": 'success'}
+
 if __name__ == "__main__":
 	app.run(debug=True)
