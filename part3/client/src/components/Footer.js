ebf0a1528030489094a3e97e11c30a245179c4f2
Qianxi Chen <qc815@nyu.edu>
2023-04-12 21:34:46 -0400

    updated footer

--- part3/client/src/components/Footer.js
+++ part3/client/src/components/Footer.js
@@ -1,7 +1,46 @@
 import styles from "./Footer.module.scss";
+import { Github } from "./UI/Icons";
 
-const Footer = (props) => {
-    return <footer className={styles.footer}></footer>;
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <section className={styles.creator}>
+                <h1>Creators</h1>
+                <div>
+                    <div>
+                        <img src="/QianxiChen.jpg" width={50} />
+                    </div>
+                    <div>
+                        <span className={styles.name}>Qianxi Chen</span>
+                        <div className={styles.item}>
+                            <Github size={20} />
+                            <a href="https://github.com/qianxichen233">
+                                Github
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div></div>
+                    <div>
+                        <span className={styles.name}>Xiaoyi Yan</span>
+                        <div className={styles.item}>
+                            <Github size={20} />
+                            <a href="https://github.com/qianxichen233">
+                                Github
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className={styles.course}>
+                <img src="/NYU-Logo.png" />
+                <h1>CS-UY 3083 Intro to Database</h1>
+                <span>Professor: Ratan Dey</span>
+                <span>Section A</span>
+            </section>
+        </footer>
+    );
 };
 
 export default Footer;
--- part3/client/src/components/UI/Icons.js
+++ part3/client/src/components/UI/Icons.js
@@ -2,8 +2,9 @@ import {
     AiOutlineStar as EmptyStar,
     AiFillStar as FilledStar,
     AiFillCaretDown as CaretDown,
+    AiFillGithub as Github,
 } from "react-icons/ai";
 
 import { CgProfile as Profile } from "react-icons/cg";
 
-export { EmptyStar, FilledStar, CaretDown, Profile };
+export { EmptyStar, FilledStar, CaretDown, Profile, Github };
