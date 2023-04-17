585a7592b0d1485494e549e3dd4fdf18865af16e
Qianxi Chen <qc815@nyu.edu>
2023-04-16 23:31:32 -0400

    fixed seat number to int

--- part3/server/apis/airplane.py
+++ part3/server/apis/airplane.py
@@ -29,7 +29,7 @@ def create_airplanes():
     if identity["type"] != "staff":
         return {"msg": "Staff Only"}, 403
 
-    if body["seat_number"] <= 0:
+    if int(body["seat_number"]) <= 0:
         return {"msg": "seat number must be positive"}, 422
 
     with getdb() as mydb:
